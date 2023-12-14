9c2923973f2e46bfd0484d096964823ac12e1af9
Abhishek <asek304@gmail.com>
2023-12-14 13:08:28 +0530

    Auction confirmation page updated

--- lib/screen/product_auction_info_screen.dart
+++ lib/screen/product_auction_info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:euvande/utilities/IconTextItem.dart';
 import 'package:flutter/material.dart';
 import 'package:euvande/screen/product_sell_journey_screen.dart';
 import 'package:euvande/utilities/constants.dart';
@@ -11,16 +12,23 @@ class ProductAuctionInfoScreen extends StatefulWidget {
 }
 
 class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
-  final yearItems = [
-    "BMW X1",
-    "BMW 7 Series",
-    "BMW 2 Series Gran Coupe",
-    "BMW X7",
-    "BMW 3 Series Gran Limousine",
-  ];
 
   bool checkboxValue = true;
+  List<IconTextItem> _items = [
+  ];
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _items.add( IconTextItem(title: '10 Km', icon: Icons.add_road));
+    _items.add( IconTextItem(title: '2009', icon: Icons.calendar_today_outlined));
+    _items.add( IconTextItem(title: '200 kW', icon: Icons.wind_power));
+    _items.add( IconTextItem(title: '10 Km', icon: Icons.add_road));
+    _items.add( IconTextItem(title: '2009', icon: Icons.calendar_today_outlined));
+    _items.add( IconTextItem(title: '200 kW', icon: Icons.wind_power));
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,7 +40,6 @@ class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
             _buildTitleSection(),
             SizedBox(height: 10),
             _buildLocationSection(),
-            SizedBox(height: 10),
             Form(
               child: FormField(
                 validator: (value) {
@@ -59,19 +66,31 @@ class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
                       ),
                     ),
                     controlAffinity: ListTileControlAffinity.leading,
-                    activeColor: Colors.blue,
+                    activeColor: Colors.black,
+                    checkColor: Colors.white,
                   );
                 },
               ),
             ),
             ElevatedButton(
-              style: raisedButtonStyle,
+              style: raisedButtonStyleForm,
+              onPressed: () {
+                // Handle button press
+              },
+              child: Text(
+                'Book Inspection',
+                style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 10,),
+            OutlinedButton(
+              style: raisedButtonStyleFormOutline,
               onPressed: () {
                 // Handle button press
               },
               child: Text(
                 'Put the car up for auction',
-                style: TextStyle(color: Colors.white, fontSize: 14),
+                style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
               ),
             ),
           ],
@@ -87,39 +106,64 @@ class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            "Mercedes-Benz E 220",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            "WDBUF56X59B412027",
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          ),
-          Container(
-            height: 250,
-            child: GridView.count(
-              crossAxisCount: 3,
-              children: List.generate(6, (index) {
-                return GestureDetector(
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          child: _buildauctionItem("10 km", Icons.add_road),
-                        ),
-                      ],
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 35.0,
+                      height: 35.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image:
+                            AssetImage("assets/logos/logo.png")),
+                      ),
                     ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Mercedes-Benz E 220",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
+                  Text(
+                    "WDBUF56X59B412027",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 20,),
+          Container(
+            child: GridView.count(
+              childAspectRatio: 4.0,
+              crossAxisCount: 3,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: List.generate(_items.length, (index) {
+                return GestureDetector(
+                  child: _buildAuctionItem(_items[index].title, _items[index].icon),
                 );
               }),
             ),
           ),
-          Text(
-            "MY DESIRED PRICE INCLUDING VAT (KC)",
-            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-          ),
+          SizedBox(height: 20,),
         ],
       ),
     );
@@ -127,8 +171,13 @@ class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
 
   Widget _buildLocationSection() {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        Text(
+          "MY DESIRED PRICE INCLUDING VAT (KC)",
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 5,),
         TextField(
           decoration: InputDecoration(
             contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
@@ -144,18 +193,14 @@ class _ProductAuctionInfoScreenState extends State<ProductAuctionInfoScreen> {
   }
 }
 
-Widget _buildauctionItem(String title, IconData iconData) {
-  return Container(
-    alignment: Alignment.center,
-    color: Colors.transparent,
-    child: Row(
-      children: [
-        Icon(iconData),
-        Text(
-          title,
-          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-        ),
-      ],
-    ),
+Widget _buildAuctionItem(String title, IconData iconData) {
+  return Row(
+    children: [
+      Icon(iconData),
+      Text(
+        title,
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+      ),
+    ],
   );
 }
